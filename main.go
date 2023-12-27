4282ab4a8bb9c4f2b33d7535fffa64307f5fdc1f
gbmoraes-dev <gbmoraes.dev@gmail.com>
2023-12-27 01:11:47 -0300

    feat: Add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/gbmoraes-dev/gopportunities/router"
+import (
+	"github.com/gbmoraes-dev/gopportunities/config"
+	"github.com/gbmoraes-dev/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	// Initialize logger
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing configs: %v", err)
+		return
+	}
+
+	// Initialize the routes
 	router.Initialize()
 }
