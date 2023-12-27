93945954a36dd3fdabddb6aedf808ef0e5d4de20
gbmoraes-dev <gbmoraes.dev@gmail.com>
2023-12-27 01:44:27 -0300

    feat: Add response schema

--- schemas/opportunity.go
+++ schemas/opportunity.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -8,8 +10,19 @@ type Opportunity struct {
 	gorm.Model
 	Role     string
 	Company  string
-	Logo     string
 	IsRemote bool
 	Link     string
 	Salary   string
+}
+
+type OpportunityResponse struct {
+	Id        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"uptadedAt"`
+	DeleteAt  time.Time `json:"deleteAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	IsRemote  bool      `json:"isRemote"`
+	Link      string    `json:"link"`
+	Salary    string    `json:"salary"`
 }
